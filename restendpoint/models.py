cc17d7014fd444f33f6b3149f563ae325b642715
Sidharth Vinod <sidharthv96@gmail.com>
2018-08-16 15:48:01 +0530

    Change mobile number to charfield

--- restendpoint/models.py
+++ restendpoint/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Pickup(models.Model):
     name = models.CharField(max_length=50, blank=True)
-    mobilenumber = models.IntegerField(blank=True)
+    mobilenumber = models.CharField(max_length=15, blank=True)
     address = models.CharField(max_length=100, blank=True)
     latt = models.DecimalField(max_digits=15, decimal_places=10)
     longitude = models.DecimalField(max_digits=15, decimal_places=10)
